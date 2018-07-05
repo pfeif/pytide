175a1aae5bffc6356d5bccd839901fc7a9e25493
pfeif <pfeif@users.noreply.github.com>
2018-07-04 23:28:57 -0400

    increase readability, reduce complexity

--- pytide.py
+++ pytide.py
@@ -13,58 +13,50 @@ STATION_ID_FILE = "./station_ids.txt"
 EMAIL_FILE = "./email_addresses.txt"
 
 
-class NOAAStation:
+class TideStation:
     '''An object that holds important NOAA tide station information like
     the station ID number, the station's name, and the low and high
     tides at the station'''
     def __init__(self, station_id, station_name=None):
-        self.sid = station_id
-        self.sname = station_name
+        self.id = station_id
+        self.name = station_name
         self.latitude = None
         self.longitude = None
         self.tide_events = []
-        self.fill_metadata()
-        self.fill_tides()
+        self._fill_empty_data()
+        # self._fill_metadata()
+        # self._fill_tides()
 
     def __str__(self):
-        output = ('ID# {0}: {1} ({2}, {3})'.format(
-            self.sid, self.sname, self.latitude, self.longitude))
+        string_output = ('ID# {0}: {1} ({2}, {3})'.format(
+            self.id, self.name, self.latitude, self.longitude))
         for tide in self.tide_events:
-            output += '\n\t{0}'.format(tide)
+            string_output += '\n\t{0}'.format(tide)
+        return string_output
 
-        return output
-
-    def request_metadata(self):
-        '''Return a dictionary containing the station's metadata'''
+    def _request_metadata(self):
+        '''Return a dictionary containing the station's metadata.'''
         # The API used here is specifically for gathering metadata about the
         # NOAA stations. It provides us with things like the name, latitude and
         # longitude of the station. For more info, see below.
         # https://tidesandcurrents.noaa.gov/mdapi/latest/
         metadata_url = ('https://tidesandcurrents.noaa.gov/mdapi/v0.6/webapi/'
-                        'stations/{0!s}.json'.format(self.sid))
+                        'stations/{0!s}.json'.format(self.id))
 
-        # use requests to get the response and turn the json into a dict
-        return requests.get(metadata_url).json()
+        # use requests to get the response and return a dict from the json
+        response = requests.get(metadata_url)
+        return response.json()
 
-    def fill_metadata(self):
-        '''Set the station's name (if not already present), the
-        station's latitude, and the station's longitude.'''
-        meta_dict = self.request_metadata()
-
-        # isn't json fun? dict->key->list->dict
-        if self.sname is None:
-            self.sname = meta_dict['stations'][0]['name']
-        self.latitude = meta_dict['stations'][0]['lat']
-        self.longitude = meta_dict['stations'][0]['lng']
-
-    def request_data(self):
+    def _request_prediction(self):
         '''Requests data from the NOAA API and returns the result in
         JSON format.'''
-        # The API requires the following fields. I've split them for the sake
+        # The API used here is for gathering tide predictions from the NOAA
+        # station. It'll give us the times and levels of the high / low tides.
+        # This API requires the following fields. I've split them for the sake
         # of convenient editing later. See below for further explanation:
         # https://tidesandcurrents.noaa.gov/api/
         base_url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
-        parameters = ['station={0!s}'.format(self.sid),
+        parameters = ['station={0!s}'.format(self.id),
                       'date=today',
                       'product=predictions',
                       'datum=MLLW',
@@ -75,33 +67,44 @@ class NOAAStation:
                       'application=PyTide']
         full_url = base_url + '&'.join(parameters)
 
-        # returns a dictionary of the parsed json
-        return requests.get(full_url).json()
+        # use requests to get the response and return a dict from the json
+        response = requests.get(full_url)
+        return response.json()
 
-    def fill_tides(self):
-        '''Fill the object's parameters with tidal data taken from a
-        dictionary.'''
-        # At this point, I expect to have a dictionary with a single key of
-        # 'predictions'. Inside of that dictionary, I should probably have 4
-        # separate dictionaries, each with a key of 'v', 'type', and 't'.
-        # 'v' will hold a string of a float for the tide level. 'type' will
-        # hold a string of either 'H' or 'L' indicating high or low tide. 't'
-        # will hold a string with the date and time of the time formatted
-        # 'YYYY-MM-DD HH:MM'.
-        predict_dict = self.request_data()
+    def _fill_empty_data(self):
+        '''This class has a number of empty attributes. It's time to
+        fill them in.'''
 
-        # The direct access would appear as something like:
-        # predict_dict['predictions'][0]['type']
+        # dictionary containing station's name, latitude, and longitude
+        meta_dict = self._request_metadata()
+
+        # dictionary containing station's tide level, tide type, and time.
+        predict_dict = self._request_prediction()
+
+        # Start filling in the metadata.
+        if self.name is None:
+            # A human-readable description is useful to have.
+            self.name = meta_dict['stations'][0]['name']
+        # Knowing the location could be pretty useful too.
+        self.latitude = meta_dict['stations'][0]['lat']
+        self.longitude = meta_dict['stations'][0]['lng']
+
+        # Start filling in the prediction data. The direct access would appear
+        # as something along the lines of:
+        #   predict_dict['predictions'][0]['type']
+        # where the 0th list entry is the first tide event, 1st would be
+        # second, and so forth.
         for event in predict_dict['predictions']:
-            tide_time = event['t']
-            tide_level = event['v']
+            tide_time = event['t']      # date/time value: 'YYYY-MM-DD HH:MM'
+            tide_level = event['v']     # water change value: '1.234'
             tide_type = 'Low'
-            if event['type'] == 'H':
+            if event['type'] == 'H':    # tide type value: 'L' or 'H'
                 tide_type = 'High'
 
             tide_string = '{0} {1} ({2})'.format(tide_time, tide_type,
                                                  tide_level)
 
+            # Append this tide to the running list.
             self.tide_events.append(tide_string)
 
 
@@ -112,16 +115,16 @@ def main():
 
     # create station objects for them, and put them in a list
     station_list = []
-    for station in station_dict:
-        sid = station
-        sname = station_dict[station]
-        station_list.append(NOAAStation(sid, sname))
+    for key in station_dict:
+        station_id = key
+        station_name = station_dict[key]
+        station_list.append(TideStation(station_id, station_name))
 
     # read the email addresses from the user's file, and put them in a set
     email_set = read_email_addresses()
 
-    for station in station_list:
-        print(station)
+    for key in station_list:
+        print(key)
         print()
 
 
@@ -130,7 +133,7 @@ def read_station_file():
     file. The keys will be the station numbers, and the values will be
     the station name. Both pieces of information come directly from the
     user's text file."""
-    stations = dict()
+    station_dict = dict()
     station_file = open(STATION_ID_FILE)
 
     # check each line in the user's text file
@@ -142,11 +145,11 @@ def read_station_file():
             # that leaves the rest as a descriptor
             station_name = line.rstrip()[8:]
             # create / update the dictionary entry
-            stations[station_id] = station_name
+            station_dict[station_id] = station_name
 
     # tidy up
     station_file.close()
-    return stations
+    return station_dict
 
 
 def read_email_addresses():
@@ -157,7 +160,9 @@ def read_email_addresses():
 
     # check each line in the email file
     for line in email_file:
+        # exclude comments and blank lines
         if not line.startswith('#') and not line.isspace():
+            # add the address to the set
             email_set.add(line)
 
     # clean up after ourselves
