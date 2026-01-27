bffc06a6661ed1eb91059db259f10053c24f40c3
James Pfeifer <pfeif@users.noreply.github.com>
2026-01-27 00:23:25 -0500

    Update tide formatting in email template

--- src/pytide/models/image.py
+++ src/pytide/models/image.py
@@ -1,14 +1,12 @@
-"""
-Class for image data
-"""
-
 from typing import NamedTuple
 
 
 class Image(NamedTuple):
-    """An image attachment for an email"""
-
     image: bytes
     content_id: str
     maintype: str = 'image'
     subtype: str = 'png'
+
+    @property
+    def content_id_normalized(self) -> str:
+        return self.content_id.strip('<>')
